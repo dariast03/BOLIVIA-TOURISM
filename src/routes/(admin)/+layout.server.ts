d916f0070d12fc7d9e8f278a42a47600bed817e1
Dante Arias <danteariastarifa@gmail.com>
2023-09-01 23:54:31 -0400

    FINALK

--- src/routes/(admin)/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import { handleLoginRedirect } from "$lib/utils"
-import { redirect } from "@sveltejs/kit"
-
-export const load = async (event: any) => {
-    console.log(event.locals.user);
-    if (!event.locals.user && !event.locals.user.rol) {
-        throw redirect(302, handleLoginRedirect(event))
-    }
-}
